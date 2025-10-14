1be9b45cc94802329d5ec799a680ab97312491e4
INTLPiva <lucas.piva@gec.inatel.br>
2025-10-14 16:24:15 -0300

    feat: vitest config and register use-case tests

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: "v8",
+      exclude: [
+        "**/node_modules/**",
+        "**/build/**",
+        "**/.{idea,git,cache,output,temp}/**",
+        "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+        "**/generated/**",
+        "**/*.spec.ts",
+        "**/*.test.ts",
+      ],
+    },
+  },
+});
